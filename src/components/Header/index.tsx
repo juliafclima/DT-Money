571305f35561482b6b3378d4a8d496506f0c2f6d
Júlia Lima <juliafclima@hotmail.com>
2024-06-14 10:56:01 -0300

    Criando um modal acessível

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
+import logoImg from "../../assets/logo.svg";
+
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>
+                Nova Transação
+              </Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
+}
